5fed0e5e108fab191005ab8d361a0b319f274c48
Vitali Semashkevich <vit-sema@yandex.ru>
2022-02-17 14:16:22 +0300

    [dev] add ui components

---
+++ src/ui/Button.vue
@@ -0,0 +1,20 @@
+<template>
+	<button class="btn">
+		<slot />
+	</button>
+</template>
+
+<script>
+export default {
+	name: 'Button',
+};
+</script>
+
+<style scoped>
+.btn {
+	padding: 10px 15px;
+	border: 1px solid teal;
+	background: none;
+	color: teal;
+}
+</style>
---
+++ src/ui/Input.vue
@@ -0,0 +1,27 @@
+<template>
+	<input type="text" class="input" :value="value" @input="updateInput" />
+</template>
+
+<script>
+export default {
+	name: 'Input',
+
+	props: {
+		value: { type: [String, Number], default: '' },
+	},
+
+	methods: {
+		updateInput(ev) {
+			this.$emit('update:value', ev.target.value);
+		},
+	},
+};
+</script>
+
+<style scoped>
+.input {
+	width: 100%;
+	border: 1px solid teal;
+	padding: 10px 15px;
+}
+</style>
